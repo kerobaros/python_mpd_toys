4c89943604c6bcc3b4d790c05ff9d867142e1dab
kerobaros <kerobaros@gmail.com>
2010-04-14 01:08:02 -0600

    Updated README, having added mpd-albumart.py and mpdrating.py

---
+++ mpd-albumart.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys,mpd,os,socket,shutil
+
+musiclocation = '/media/FUZZBOOK/Music/FLAC' #location of music       
+artlocation = '~/.covers' #where you want albumart storing
+tmp_path = '/tmp/mpd.jpg' #location for temporary file so conky can find it
+
+musiclocation = os.path.expanduser(musiclocation)
+artlocation = os.path.expanduser(artlocation)
+
+client = mpd.MPDClient()  
+try:
+    client.connect("localhost", 6600)    
+except socket.error:
+    sys.exit(1)
+    
+song = client.currentsong() 
+artist = song['artist']
+album = song['album']
+
+covername = os.path.join( artlocation, artist+'-'+album+'.jpg')
+covername = covername.replace(' ',' ')
+fullmusiclocation = os.path.split(os.path.join(musiclocation, song['file']))[0]
+
+try:
+    os.remove(tmp_path)    
+except OSError: pass
+
+if os.path.exists(os.path.join(fullmusiclocation, 'front.jpg')):
+    #print 'front.jpg exists, symlinking...'
+    os.symlink(os.path.join(fullmusiclocation, 'front.jpg'), tmp_path)
+    shutil.copy(os.path.join(fullmusiclocation, 'front.jpg'), covername)
+    sys.exit(0)
+if os.path.exists(os.path.join(fullmusiclocation, 'folder.jpg')):
+    #print 'folder.jpg exists, symlinking...'
+    os.symlink(os.path.join(fullmusiclocation, 'folder.jpg'), tmp_path)
+    shutil.copy(os.path.join(fullmusiclocation, 'folder.jpg'), covername)
+    sys.exit(0)
+    
---
+++ mpdrating.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mpd, os, sys
+
+method = ''
+if len(sys.argv) > 1: method = sys.argv[1]
+
+ratings = {}
+ratingsfilename = os.path.expanduser("~/.mpd/ratings.db")
+ratingsfile = open(ratingsfilename)
+
+for line in ratingsfile.readlines():
+	if line != '': 
+		linefile, linerating = line.rstrip('\n').split(':')
+		ratings[linefile] = linerating
+
+ratingsfile.close()
+
+client = mpd.MPDClient()
+try: client.connect("localhost", 6600)
+except socket.error:
+	print 'Couldn\'t connect to MPD!'
+	sys.exit(1)
+
+song = client.currentsong()
+	
+songfilename = song['file']
+
+
+if method != '':
+	if method == '1': ratings[songfilename] = 1
+	if method == '2': ratings[songfilename] = 2
+	if method == '3': ratings[songfilename] = 3
+	if method == '4': ratings[songfilename] = 4
+	if method == '5': ratings[songfilename] = 5
+	if method == '-':
+	    print '*' * int(ratings.get(songfilename, 0))
+	    sys.exit(0)
+
+print ratings.get(songfilename, 0)
+
+ratingsfile = open(ratingsfilename, 'w')
+for rating in ratings.items():
+	ratingsfile.write(rating[0] + ':' + str(rating[1]) + '\n')
+
+ratingsfile.close()
