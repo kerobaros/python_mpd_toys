d803db44498c0044d0a2520c3bc1ac6007deec31
kerobaros <kerobaros@gmail.com>
2010-04-14 01:18:31 -0600

    mpdrating: added skip-on-one-star capability, updated README to match

--- mpdrating.py
+++ mpdrating.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# change following line from True to False to prevent autoskipping one-starred songs
+skip_on_one = True
+
 import mpd, os, sys
 
 method = ''
@@ -11,37 +14,39 @@ ratingsfilename = os.path.expanduser("~/.mpd/ratings.db")
 ratingsfile = open(ratingsfilename)
 
 for line in ratingsfile.readlines():
-	if line != '': 
-		linefile, linerating = line.rstrip('\n').split(':')
-		ratings[linefile] = linerating
+    if line != '': 
+        linefile, linerating = line.rstrip('\n').split(':')
+        ratings[linefile] = linerating
 
 ratingsfile.close()
 
 client = mpd.MPDClient()
 try: client.connect("localhost", 6600)
 except socket.error:
-	print 'Couldn\'t connect to MPD!'
-	sys.exit(1)
+    print 'Couldn\'t connect to MPD!'
+    sys.exit(1)
 
 song = client.currentsong()
-	
+    
 songfilename = song['file']
 
 
 if method != '':
-	if method == '1': ratings[songfilename] = 1
-	if method == '2': ratings[songfilename] = 2
-	if method == '3': ratings[songfilename] = 3
-	if method == '4': ratings[songfilename] = 4
-	if method == '5': ratings[songfilename] = 5
-	if method == '-':
-	    print '*' * int(ratings.get(songfilename, 0))
-	    sys.exit(0)
+    if method == '1':
+        ratings[songfilename] = 1
+        if skip_on_one: client.next()
+    if method == '2': ratings[songfilename] = 2
+    if method == '3': ratings[songfilename] = 3
+    if method == '4': ratings[songfilename] = 4
+    if method == '5': ratings[songfilename] = 5
+    if method == '-':
+        print '*' * int(ratings.get(songfilename, 0))
+        sys.exit(0)
 
 print ratings.get(songfilename, 0)
 
 ratingsfile = open(ratingsfilename, 'w')
 for rating in ratings.items():
-	ratingsfile.write(rating[0] + ':' + str(rating[1]) + '\n')
+    ratingsfile.write(rating[0] + ':' + str(rating[1]) + '\n')
 
 ratingsfile.close()
