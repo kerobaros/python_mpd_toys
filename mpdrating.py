083fbd479720178abefcf450af543568102cb010
kerobaros <kerobaros@gmail.com>
2014-10-16 17:08:57 -0500

    Updating, updating, updating. Starting to use python-mpd2 instead. Explicitly call python2 now, will port to python3 eventually.

--- mpdrating.py
+++ mpdrating.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 # change following line from True to False to prevent autoskipping one-starred songs
--- random_album.py
+++ random_album.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import random, mpd
+import random
+from mpd import MPDClient
 from optparse import OptionParser
 
-def queue_album(artist = '', year = '', debug = False):
+def queue_album(MPD, artist = '', year = '', debug = False):
 
-    MPD = mpd.MPDClient()
-    MPD.connect("localhost",6600)
     if artist != '': list = sorted(MPD.list("album", "artist", artist))
     elif year != '': list = sorted(MPD.list("album", "date", year))
     else: list = sorted(MPD.list("album"))
@@ -21,11 +20,22 @@ def queue_album(artist = '', year = '', debug = False):
             for track in tracks: MPD.add(track)
             MPD.play()
             print 'Now Playing:', MPD.currentsong()['artist'], '-', MPD.currentsong()['album'], '(%s)' % MPD.currentsong()['date']
-        MPD.close()
-        MPD.disconnect()
     else: print 'No albums matching search!'
 
+def connect():
+
+    MPD = MPDClient()
+    MPD.connect("localhost", 6600)
+    return MPD
+
+def disconnect(MPD):
+    MPD.close()
+    MPD.disconnect()
+
 def main():
+
+    mpd = connect()
+
     random.seed()
     parser = OptionParser()
     parser.add_option("-a", "--artist", type = "string", dest = "artist", help = "Specifies an artist to limit album choices from; needs to be a quoted string")
@@ -37,19 +47,21 @@ def main():
     i = 0
 
     if options.debug == None: 
-        MPD.stop()
-        MPD.clear()
+        mpd.stop()
+        mpd.clear()
     
     if options.iterations == None:
-        if options.artist: queue_album(artist = options.artist, debug = options.debug)
-        elif options.year: queue_album(year = options.year, debug = options.debug)
-        else: queue_album(debug = options.debug)
+        if options.artist: queue_album(MPD = mpd, artist = options.artist, debug = options.debug)
+        elif options.year: queue_album(MPD = mpd, year = options.year, debug = options.debug)
+        else: queue_album(MPD = mpd, debug = options.debug)
     else:
         while i < options.iterations:
-            if options.artist: queue_album(artist = options.artist, debug = options.debug)
-            elif options.year: queue_album(year = options.year, debug = options.debug)
-            else: queue_album(debug = options.debug)
+            if options.artist: queue_album(MPD = mpd, artist = options.artist, debug = options.debug)
+            elif options.year: queue_album(MPD = mpd, year = options.year, debug = options.debug)
+            else: queue_album(MPD = mpd, debug = options.debug)
             i += 1
         
+    disconnect(mpd)
+
 if __name__ == '__main__':
     main()
